6ae470bc88855efd5b927587e1a83b4e0ed3d282
oboreo <boris@southpawtech.com>
2013-09-29 23:36:17 -0400

    BRANCH 4.1:   updated Card Layout to resolve a transparency issue when adding new notes

--- src/tactic/ui/panel/tool_layout_wdg.py
+++ src/tactic/ui/panel/tool_layout_wdg.py
@@ -182,30 +182,33 @@ class ToolLayoutWdg(FastTableLayoutWdg):
             '''
         } )
 
+        main_bg1 = layout_wdg.get_color("background")
+        main_bg2 = layout_wdg.get_color("background", 5)
+
         bg1 = layout_wdg.get_color("background3")
         bg2 = layout_wdg.get_color("background3", 5)
         layout_wdg.add_relay_behavior( {
             'type': 'mouseover',
             'bvr_match_class': 'spt_item_top',
             'cbjs_action': '''
-            bvr.src_el.setStyle("opacity", "0.8");
+            bvr.src_el.setStyle('background', '%s');
             var el = bvr.src_el.getElement(".spt_tile_title");
             if (el) {
                 el.setStyle("background", "%s");
             }
-            ''' % bg2
+            ''' % (main_bg2, bg2)
         } )
 
         layout_wdg.add_relay_behavior( {
             'type': 'mouseout',
             'bvr_match_class': 'spt_item_top',
             'cbjs_action': '''
-            bvr.src_el.setStyle("opacity", "1.0");
+            bvr.src_el.setStyle('background', '%s');
             var el = bvr.src_el.getElement(".spt_tile_title");
             if (el) {
                 el.setStyle("background", "%s");
             }
-            ''' % bg1
+            ''' %(main_bg1, bg1)
         } )
 
 
