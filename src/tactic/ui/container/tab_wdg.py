0926628a282db3e2317dca651ab03df7bc1088df
ryandzy <stuart7days@hotmail.com>
2013-11-23 05:31:09 +0800

    BRANCH 4.1:  - Edited security_type to set pre-checked box based on the Security Access Levels chart.  - Using spt.alert instead of alert in tab_wdg

--- src/tactic/ui/container/tab_wdg.py
+++ src/tactic/ui/container/tab_wdg.py
@@ -1460,7 +1460,7 @@ spt.tab.header_drag_action = function( evt, bvr, mouse_411) {
                 xml += '  </display>\n';
                 xml += '</element>\n';
 
-                alert(xml);
+                spt.alert(xml);
 
                 '''
             } )
