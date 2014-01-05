b81d553f9940975144ac99e0818a9a7136f8fb44
Remko Noteboom <remko@southpawtech.com>
2014-01-04 21:46:29 -0500

    BRANCH 4.1-DEV: cleaned up the notes input widget a bit.

--- src/tactic/ui/input/note_input_wdg.py
+++ src/tactic/ui/input/note_input_wdg.py
@@ -13,12 +13,12 @@
 __all__ = ['NoteInputWdg', 'NoteHistoryWdg']
 
 from pyasm.biz import Project
-from pyasm.web import DivWdg
+from pyasm.web import DivWdg, Table
 from pyasm.widget import BaseInputWdg, SelectWdg, TextWdg, IconButtonWdg, IconWdg, HiddenWdg, TextAreaWdg
 
 from pyasm.common import TacticException
 
-from pyasm.search import Search
+from pyasm.search import Search, SearchType
 
 from tactic.ui.common import BaseRefreshWdg
 
@@ -29,8 +29,12 @@ class NoteInputWdg(BaseInputWdg):
     def get_display(my):
 
         top = DivWdg()
+        name = my.get_name()
+        top.add_class("spt_note_input_top")
 
         context = my.get_option("context")
+        if not context:
+            context = name
 
         sobject = my.get_option("sobject")
         if not sobject:
@@ -44,36 +48,86 @@ class NoteInputWdg(BaseInputWdg):
             search = Search("sthpw/note") 
             #search.add_relationship_filters(my.filtered_parents, type='hierarchy')
             search.add_parent_filter(sobject)
+            search.add_filter("context", context)
             search.add_order_by("process")
             search.add_order_by("context")
             search.add_order_by("timestamp desc")
+            count = search.get_count()
             last_note = search.get_sobject()
         else:
             last_note = None
+            count = 0
 
+        if not last_note:
+            last_note = SearchType.create("sthpw/note")
+            last_note.set_value("login", "")
+            last_note.set_value("timestamp", "")
+            last_note.set_value("note", "")
 
         if last_note:
             last_div = DivWdg()
             top.add(last_div)
 
+            table = Table()
+            table.add_style("width: 100%")
+            table.add_attr("cellpadding", "0px")
+            table.add_attr("cellspacing", "0px")
+            last_div.add(table)
+            table.add_row()
+            td = table.add_cell()
+            td.add_style("vertical-align: top")
+            td.add_style("padding: 10px 15px 10px 5px")
+            table.add_border()
+            table.add_color("background", "background", -5)
+
             note_str = last_note.get_value("note")
             login = last_note.get_value("login")
             if not login:
-                login = "<i style='opacity: 0.3'>unknown</i>"
+                login = "unknown"
             date = last_note.get_datetime_value("timestamp")
-            date_str = date.strftime("%Y-%m-%d")
+            if date:
+                date_str = "<i style='font-size: 0.8em'>%s</i>" % date.strftime("%Y-%m-%d")
+            else:
+                date_str = ""
 
-            last_div.add("%s - %s<br/>" % (login, date_str))
+            login = "<i style='opacity: 0.3'>%s</i>" % login
+            td.add("%s<br/>%s<br/>" % (login, date_str))
 
             note_str_div = DivWdg()
-            last_div.add(note_str_div)
             note_str_div.add(note_str)
-            note_str_div.add_style("padding: 15px")
 
-            last_div.add_style("padding: 5px")
+            td = table.add_cell( note_str_div )
+            td.add_style("vertical-align: top")
+            td.add_style("padding: 10px 15px 10px 10px")
+
+            """
+            td.add_behavior( {
+                'type': 'click_up',
+                'cbjs_action': '''
+                var top = bvr.src_el.getParent(".spt_note_input_top");
+                var text_el = top.getElement(".spt_add_entry");
+                text_el.setStyle("display", "");
+
+                '''
+            } )
+            """
 
 
-            last_div.add_behavior( {
+
+
+            # log
+            if count == 0:
+                td = table.add_cell( "" )
+            elif count == 1:
+                td = table.add_cell( "<i style='font-size: 0.8em'>More...><br/>(%s entry)</i>" % count )
+            else:
+                td = table.add_cell( "<i style='font-size: 0.8em'>More...><br/>(%s entries)</i>" % count )
+            td.add_style("vertical-align: top")
+            td.add_style("padding: 3px")
+            td.add_class("hand")
+
+
+            td.add_behavior( {
                 'type': 'click_up',
                 'search_key': search_key,
                 'cbjs_action': '''
@@ -88,14 +142,19 @@ class NoteInputWdg(BaseInputWdg):
             } )
 
 
+
         name = my.get_input_name()
         text = TextAreaWdg(name)
         top.add(text)
+        text.add_style("width: 100%")
+        text.add_class("spt_add_entry")
 
+        """
         if search_key and not sobject.is_insert():
             from tactic.ui.widget import DiscussionWdg
             discussion_wdg = DiscussionWdg(search_key=search_key, context_hidden=False, show_note_expand=False, show_add=False)
             top.add(discussion_wdg)
+        """
 
         return top
 
