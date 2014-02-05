7b9d0ef3c63353ad6b44fc54a63ed4486dd16ae5
remkonoteboom <remko@southpawtech.com>
2014-02-05 17:59:29 +0000

    BRANCH 4.2-DEV: set to not record transaction on redo

--- src/pyasm/command/undo_cmd.py
+++ src/pyasm/command/undo_cmd.py
@@ -180,8 +180,9 @@ class RedoCmd(Command):
         my.transaction_log.set_value("type", "undo")
         my.transaction_log.commit()
 
-        # get the transaction
+        # get the transaction and set it to not record this transaction
         transaction = Transaction.get()
+        transaction.set_record_flag(false)
 
         # update the change timestamps
         transaction.update_change_timestamps(my.transaction_log)
