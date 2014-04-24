113604985ef361989ce77be07c2d9618e022abc1
Remko Noteboom <remko@southpawtech.com>
2014-04-24 19:00:23 -0400

    BRANCH 4.2-DEV: added improvements to the gallery

--- src/tactic/ui/widget/gallery_wdg.py
+++ src/tactic/ui/widget/gallery_wdg.py
@@ -26,6 +26,8 @@ class GalleryWdg(BaseRefreshWdg):
 
     def get_display(my):
 
+        my.sobject_data = {}
+
         top = my.top
         top.add_style
         top.add_class("spt_gallery_top")
@@ -54,7 +56,7 @@ class GalleryWdg(BaseRefreshWdg):
         icon = IconWdg(title="Close", icon="/plugins/remington/pos/icons/close.png")
         inner.add(icon)
         icon.add_style("position: absolute")
-        icon.add_style("cursor: hand")
+        icon.add_style("cursor: pointer")
         icon.add_style("bottom: 0px")
         icon.add_style("right: 0px")
         icon.add_behavior( {
@@ -68,7 +70,7 @@ class GalleryWdg(BaseRefreshWdg):
 
         icon = IconWdg(title="Previous", icon="/plugins/remington/pos/icons/chevron_left.png")
         inner.add(icon)
-        icon.add_style("cursor: hand")
+        icon.add_style("cursor: pointer")
         icon.add_style("position: absolute")
         icon.add_style("top: 40%")
         icon.add_style("left: 0px")
@@ -100,10 +102,13 @@ class GalleryWdg(BaseRefreshWdg):
             width = 1024
 
 
+        paths = my.get_paths()
 
+        descriptions = [ my.sobject_data.get(x).get("description") for x in paths]
         inner.add_behavior( {
         'type': 'load',
         'width': width,
+        'descriptions': descriptions,
         'cbjs_action': '''
 
         spt.gallery = {};
@@ -111,6 +116,10 @@ class GalleryWdg(BaseRefreshWdg):
         spt.gallery.top = bvr.src_el;
         spt.gallery.width = bvr.width;
         spt.gallery.content = spt.gallery.top.getElement(".spt_gallery_content");
+        spt.gallery.desc_el = spt.gallery.top.getElement(".spt_gallery_description");
+        spt.gallery.descriptions = bvr.descriptions;
+        spt.gallery.index = 0;
+        spt.gallery.total = bvr.descriptions.length;
 
         spt.gallery.init = function() {
             
@@ -124,19 +133,19 @@ class GalleryWdg(BaseRefreshWdg):
 
 
         spt.gallery.show_next = function() {
-            var width = spt.gallery.width;
-            var content = spt.gallery.content;
-            var pos = content.getStyle("margin-left");
-            pos = parseInt(pos.replace("px", ""))
-            new Fx.Tween(content,{duration: 250}).start("margin-left", pos-width);
+            if (spt.gallery.index == spt.gallery.total-1) {
+                return;
+            }
+            spt.gallery.index += 1;
+            spt.gallery.show_index(spt.gallery.index);
         }
 
         spt.gallery.show_prev = function() {
-            var width = spt.gallery.width;
-            var content = spt.gallery.content;
-            var pos = content.getStyle("margin-left");
-            pos = parseInt(pos.replace("px", ""))
-            new Fx.Tween(content,{duration: 250}).start("margin-left", pos+width);
+            if (spt.gallery.index == 0) {
+                return;
+            }
+            spt.gallery.index -= 1;
+            spt.gallery.show_index(spt.gallery.index);
         }
 
 
@@ -144,7 +153,20 @@ class GalleryWdg(BaseRefreshWdg):
             var width = spt.gallery.width;
             var margin = - width * index;
             var content = spt.gallery.content;
-            content.setStyle("margin-left", margin + "px");
+            //content.setStyle("margin-left", margin + "px");
+            new Fx.Tween(content,{duration: 250}).start("margin-left", margin);
+
+            spt.gallery.index = index;
+
+            var description = spt.gallery.descriptions[index];
+            var total = spt.gallery.total-1;
+            if (!description) {
+                description = "("+index+" of "+total+")";
+            }
+            else {
+                description = "("+(index+1)+" of "+total+") - " + description;
+            }
+            spt.gallery.set_description(description);
         }
 
 
@@ -153,11 +175,38 @@ class GalleryWdg(BaseRefreshWdg):
             var top = content.getParent(".spt_gallery_top");
             spt.behavior.destroy_element(top);
         }
+
+
+        spt.gallery.set_description = function(desc) {
+            var desc_el = spt.gallery.desc_el;
+            desc_el.innerHTML = desc;
+        }
+
         '''
         } )
 
 
 
+
+        desc_div = DivWdg()
+        desc_div.add_class("spt_gallery_description")
+        desc_div.add_style("height: 30px")
+        desc_div.add_style("width: %s" % width)
+        desc_div.add_style("text-align: center")
+        desc_div.add_style("background: rgba(255,255,255,0.8)")
+        desc_div.add_style("color: #000")
+        desc_div.add_style("font-weight: bold")
+        desc_div.add_style("font-size: 1.2em")
+        desc_div.add_style("padding-top: 3px")
+        desc_div.add_style("margin-left: -%s" % (width/2))
+        desc_div.add("")
+
+        desc_outer_div = DivWdg()
+        inner.add(desc_outer_div)
+        desc_outer_div.add_style("position: fixed")
+        desc_outer_div.add(desc_div)
+        desc_outer_div.add_style("bottom: 0px")
+        desc_outer_div.add_style("left: 50%")
 
 
 
@@ -178,8 +227,6 @@ class GalleryWdg(BaseRefreshWdg):
         scroll.add_style("margin-left: auto")
         scroll.add_style("margin-right: auto")
 
-
-        paths = my.get_paths()
 
 
         total_width = width * len(paths)
@@ -265,10 +312,12 @@ class GalleryWdg(BaseRefreshWdg):
             #img.add_style("width: 100%")
 
 
+
         content.add_behavior({
             'type': 'load',
             'index': curr_index,
             'cbjs_action': '''
+            if (!bvr.index) bvr.index = 0;
             spt.gallery.show_index(bvr.index);
             '''
         } )
@@ -297,6 +346,11 @@ class GalleryWdg(BaseRefreshWdg):
             snapshots = Snapshot.get_by_sobjects(sobjects)
             file_objects = File.get_by_snapshots(snapshots, file_type='main')
             paths = [x.get_web_path() for x in file_objects]
+
+            for sobject, path in zip(sobjects, paths):
+                my.sobject_data[path] = sobject
+
+                
 
         elif paths:
             return paths
