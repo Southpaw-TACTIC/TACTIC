9fa13c34d89787e6f90b065e5054b72dccfc7878
oboreo <boris@southpawtech.com>
2014-06-02 12:06:02 -0400

    BRANCH 4.2-105:   made Common.get_dir_info() use unicode on the directory so it will read the files in it as unicode strings   fixed a typo in client api's execute_transaction   added IndexError except for GalleryWdg

--- src/pyasm/common/common.py
+++ src/pyasm/common/common.py
@@ -462,14 +462,19 @@ class Common(Base):
         elif os.path.isdir(dir):
             # this part is too slow
             if not skip_dir_details:
-                for (path, dirs, files) in os.walk(dir):
+                for (path, dirs, files) in os.walk(unicode(dir)):
                     for file in files:
                         filename = os.path.join(path, file)
                         if os.path.islink(filename):
                             # ignore links
                             pass                        
                         else:
-                            dir_size += os.path.getsize(filename)
+                            try: 
+                                dir_size += os.path.getsize(filename)
+                            except:
+                                continue
+
+                                
                         count += 1
             file_type = 'directory'
         else:
@@ -954,10 +959,14 @@ class Common(Base):
         import sys
         python = sys.executable
         # for windows
+        print "Restarting the process. . ."
+        print
         python = python.replace('\\','/')
         if os.name =='nt':
             import subprocess
-            subprocess.Popen([python, sys.argv])
+            cmd_list = [python]
+            cmd_list.extend(sys.argv)
+            subprocess.Popen(cmd_list)
             pid = os.getpid()
             kill = KillProcessThread(pid)
             kill.start()
@@ -974,13 +983,13 @@ class KillProcessThread(threading.Thread):
 
     def run(my):
         """kill function for Win32 prior to Python2.7"""
-        import time
-        # pause for the DbConfigSaveCbk to finish first
-        time.sleep(2)
+       
         import ctypes
         kernel32 = ctypes.windll.kernel32
         handle = kernel32.OpenProcess(1, 0, my.pid)
-        return (0 != kernel32.TerminateProcess(handle, 0))
+        kernel32.TerminateProcess(handle, -1)
+        rtn = kernel32.CloseHandle(handle)
+        return (0 != rtn)
 
 
 gl = globals()
