715eb4d8222864ca0b383df4376b041eb24ac4c7
Remko Noteboom <remko@southpawtech.com>
2015-07-22 02:32:06 -0400

    BRANCH 4.4: added test for my tasks detailing

--- src/tactic/ui/table/sobject_detail_wdg.py
+++ src/tactic/ui/table/sobject_detail_wdg.py
@@ -146,65 +146,6 @@ class SObjectDetailElementWdg(BaseTableElementWdg):
         return div
 
 
-    """
-    def get_link_wdg(my, target_id, title, widget=None):
-
-        sobject = my.get_current_sobject()
-
-        path = "/%s" % my.search_key
-        options = {
-            'path': path,
-            'class_name': 'tactic.ui.panel.SObjectPanelWdg',
-            #'class_name': 'tactic.ui.panel.SearchTypePanelWdg',
-            'search_key': my.search_key
-        }
-
-        security = Environment.get_security()
-        if not security.check_access("url", path, "view"):
-            return
-        options['path'] = path
-        
-
-        view_link_wdg = DivWdg(css="hand")
-        view_link_wdg.add_style( "padding-top: 5px" )
-
-        if widget:
-            view_link_wdg.add(widget)
-        else:
-            view_link_wdg.add(title)
-
-
-        # put in a default class name
-        if not options.get('class_name'):
-            options['class_name'] = "tactic.ui.panel.ViewPanelWdg"
-
-        # put in a default search
-        if not options.get('filters'):
-            options['filters'] = '0';
-
-        behavior = {
-            'type':         'click_up',
-            'cbfn_action':  'spt.side_bar.display_link_cbk',
-            'target_id':    target_id,
-            'is_popup':     'true',
-            'options':      options,
-        }
-        view_link_wdg.add_behavior( behavior )
-
-        # use shift click to open up in a popup
-        behavior = {
-            'type':         'click_up',
-            'mouse_btn':    'LMB',
-            'modkeys':      'SHIFT',
-            'cbfn_action':  'spt.side_bar.display_link_cbk',
-            'target_id':    target_id, # FIXME: has to be here for now
-            'title':        sobject.get_code(),
-            'is_popup':     'false',
-            'options':      options,
-        }
-        view_link_wdg.add_behavior( behavior )
-        return view_link_wdg
-    """
 
 
 __all__.append("SObjectTaskStatusElementWdg")
@@ -219,21 +160,10 @@ class SObjectTaskStatusElementWdg(SObjectDetailElementWdg):
 
         sobject = my.get_current_sobject()
 
-        use_parent = my.get_option("use_parent")
-        use_parent = use_parent in ['true', True]
-        #if use_parent in ['true', True]:
-        #    sobject = sobject.get_parent()
-        #    if not sobject:
-        #        return DivWdg()
-
         my.search_key = SearchKey.get_by_sobject(sobject)
     
         div = DivWdg()
         div.add_class("hand")
-        #div.add_style("width: 100%")
-        #div.add_style("height: 100%")
-
-        target_id = "main_body"
 
         title = "Show Item Details"
         if my.widget:
@@ -254,10 +184,6 @@ class SObjectTaskStatusElementWdg(SObjectDetailElementWdg):
         widget.add_behavior( {
         'type': 'click_up',
         'search_key': my.search_key,
-        'use_parent': use_parent,
-        'tab_element_names': tab_element_names,
-        'detail_view': detail_view,
-        'show_task_process': my.show_task_process,
         'code': code,
         'name': name,
         'cbjs_action': '''
@@ -265,10 +191,6 @@ class SObjectTaskStatusElementWdg(SObjectDetailElementWdg):
         var class_name = 'tactic.ui.tools.SObjectTaskStatusDetailWdg';
         var kwargs = {
             search_key: bvr.search_key,
-            use_parent: bvr.use_parent,
-            tab_element_names: bvr.tab_element_names,
-            show_task_process: bvr.show_task_process,
-            detail_view: bvr.detail_view
         };
 
         var mode = '';
@@ -293,8 +215,75 @@ class SObjectTaskStatusElementWdg(SObjectDetailElementWdg):
         } )
 
 
-        #link_wdg = my.get_link_wdg(target_id, title, widget)
-        #div.add( link_wdg )
+        div.add(widget)
+
+        return div
+
+
+__all__.append("TaskDetailPanelElementWdg")
+class TaskDetailPanelElementWdg(SObjectDetailElementWdg):
+    '''The element widget that displays according to type'''
+
+    ARGS_KEYS = {
+    }
+ 
+
+    def get_display(my):
+
+        sobject = my.get_current_sobject()
+
+        my.search_key = SearchKey.get_by_sobject(sobject)
+    
+        div = DivWdg()
+        div.add_class("hand")
+
+        title = "Show Item Details"
+        if my.widget:
+            widget = my.widget
+        else:
+            widget = IconButtonWdg(title=title, icon=IconWdg.ZOOM)
+
+
+        code = sobject.get_code()
+        name = sobject.get_value("name", no_exception=True)
+        if not name:
+            name = code
+
+
+        widget.add_behavior( {
+        'type': 'click_up',
+        'search_key': my.search_key,
+        'code': code,
+        'name': name,
+        'cbjs_action': '''
+        spt.tab.set_main_body_tab();
+        var class_name = 'tactic.ui.tools.TaskDetailPanelWdg';
+        var kwargs = {
+            search_key: bvr.search_key,
+        };
+
+        var mode = '';
+        var layout = bvr.src_el.getParent(".spt_tool_top");
+        if (layout != null) {
+            mode = 'tool'
+        }
+
+        if (mode == 'tool') {
+            spt.app_busy.show("Loading ...");
+            var layout = bvr.src_el.getParent(".spt_tool_top");
+            var element = layout.getElement(".spt_tool_content");
+            spt.panel.load(element, class_name, kwargs);
+            spt.app_busy.hide();
+        }
+        else {
+            var element_name = "detail_"+bvr.code;
+            var title = "Detail ["+bvr.name+"]";
+            spt.tab.add_new(element_name, title, class_name, kwargs);
+        }
+        '''
+        } )
+
+
         div.add(widget)
 
         return div
