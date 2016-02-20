a837782a1c47a04d0e95592be4ec1135596c9d86
Remko Noteboom <remko@southpawtech.com>
2016-02-20 10:35:13 -0500

    BRANCH 4.5: added a pluralize function

--- src/pyasm/common/common.py
+++ src/pyasm/common/common.py
@@ -1085,6 +1085,131 @@ class Common(Base):
     total_size = staticmethod(total_size)
 
 
+
+
+    ABERRANT_PLURAL_MAP = {
+        'appendix': 'appendices',
+        'barracks': 'barracks',
+        'cactus': 'cacti',
+        'child': 'children',
+        'corpora': 'corpus',
+        'criterion': 'criteria',
+        'deer': 'deer',
+        'echo': 'echoes',
+        'elf': 'elves',
+        'embargo': 'embargoes',
+        'focus': 'foci',
+        'foot': 'feet',
+        'fungus': 'fungi',
+        'goose': 'geese',
+        'half': 'halves',
+        'hero': 'heroes',
+        'hoof': 'hooves',
+        'index': 'indices',
+        'knife': 'knives',
+        'leaf': 'leaves',
+        'life': 'lives',
+        'loaf': 'loaves',
+        'man': 'men',
+        'matrix': 'matrices',
+        'mouse': 'mice',
+        'nucleus': 'nuclei',
+        'person': 'people',
+        'phenomenon': 'phenomena',
+        'potato': 'potatoes',
+        'radius': 'radii',
+        'self': 'selves',
+        'sky': 'skies',
+        'syllabus': 'syllabi',
+        'tomato': 'tomatoes',
+        'tooth': 'teeth',
+        'torpedo': 'torpedoes',
+        'veto': 'vetoes',
+        'wife': 'wives',
+        'wolf': 'wolves',
+        'woman': 'women',
+        }
+
+    VOWELS = set('aeiou')
+
+    def pluralize(cls, singular):
+        """Return plural form of given lowercase singular word (English only). Based on
+        ActiveState recipe http://code.activestate.com/recipes/413172/
+        
+        >>> pluralize('')
+        ''
+        >>> pluralize('goose')
+        'geese'
+        >>> pluralize('dolly')
+        'dollies'
+        >>> pluralize('genius')
+        'genii'
+        >>> pluralize('jones')
+        'joneses'
+        >>> pluralize('pass')
+        'passes'
+        >>> pluralize('zero')
+        'zeros'
+        >>> pluralize('casino')
+        'casinos'
+        >>> pluralize('hero')
+        'heroes'
+        >>> pluralize('church')
+        'churches'
+        >>> pluralize('x')
+        'xs'
+        >>> pluralize('car')
+        'cars'
+
+        """
+        if singular != singular.lower():
+            is_title = True
+        else:
+            is_title = False
+        singular = singular.lower()
+
+        if not singular:
+            return ''
+        plural = cls.ABERRANT_PLURAL_MAP.get(singular)
+        if plural:
+            if is_title:
+                return plural.title()
+            else:
+                return plural
+        root = singular
+        try:
+            if singular[-1] == 'y' and singular[-2] not in VOWELS:
+                root = singular[:-1]
+                suffix = 'ies'
+            elif singular[-1] == 's':
+                if singular[-2] in VOWELS:
+                    if singular[-3:] == 'ius':
+                        root = singular[:-2]
+                        suffix = 'i'
+                    else:
+                        root = singular[:-1]
+                        suffix = 'ses'
+                else:
+                    suffix = 'es'
+            elif singular[-2:] in ('ch', 'sh'):
+                suffix = 'es'
+            else:
+                suffix = 's'
+        except IndexError:
+            suffix = 's'
+        plural = root + suffix
+
+        if is_title:
+            plural = plural.title()
+
+        return plural
+
+    pluralize = classmethod(pluralize)
+
+
+
+
+
     # take from: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
     def which(program):
         def is_exe(fpath):
