209f7927b71bd5a2ebd6cfe751b4c8b43a0d850b
menechel <michael.menechella@southpawtech.com>
2016-05-12 13:21:25 -0400

    WORK-418: - In the startup view changed the login_group column to name - Set the name field for the edit view and titled it Group Name - Added an edit definition for the name field - Changed the trigger event for LoginGroupTrigger to change instead of update - Modified LoginGroupTrigger to accommodate for insert and update events and due to changing name field on edit

--- src/tactic/command/login_group_trigger.py
+++ src/tactic/command/login_group_trigger.py
@@ -22,13 +22,36 @@ class LoginGroupTrigger(Trigger):
     def execute(my):
 
         input = my.get_input()
-        search_key = input.get("search_key")
+        mode = input.get("mode")
 
+        if mode not in ['insert', 'update']:
+            return
+
+        search_key = input.get("search_key")
         sobj = SearchKey.get_by_search_key(search_key)
-        login_group = sobj.get_value('login_group')
+
+        if mode == "insert":
+            login_group_name = sobj.get_value('login_group')
+
+            if not login_group_name:
+                raise Exception("A Group Name has not been entered. Please enter a Group Name.")
+
+            sobj.set_value('name', login_group_name)
+
+        elif mode == "update":
+            login_group_name = sobj.get_value('name')
+
+            if not login_group_name:
+                login_group_name = sobj.get_value('login_group')
+
+        login_group = login_group_name.lower()
+        login_group = login_group.replace(" ", "_")
+
+        sobj.set_value('login_group', login_group)
         sobj.set_value('code', login_group)
 
         sobj.commit(triggers=False)
+
 
 
 
