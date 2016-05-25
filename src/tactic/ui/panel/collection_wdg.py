2c20f696d8f41d671ae9f505656f9796c6183b02
Sean Hsu <sean.hsu@southpawtech.com>
2016-05-25 14:01:18 -0400

    WORK-446: - removed semi colon

--- src/tactic/ui/panel/collection_wdg.py
+++ src/tactic/ui/panel/collection_wdg.py
@@ -935,7 +935,7 @@ class CollectionContentWdg(BaseRefreshWdg):
         top = my.top
         top.add_style("min-height: 400px")
 
-        my.kwargs["scale"] = 75;
+        my.kwargs["scale"] = 75
         my.kwargs["show_scale"] = True
         my.kwargs["expand_mode"] = "plain"
         my.kwargs["show_search_limit"] = False
