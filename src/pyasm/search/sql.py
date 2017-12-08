73ed0e240ba10b8fcd68b551ce27fa4eab336576
TACTIC <tactic@localhost.localdomain>
2017-12-08 11:48:48 -0500

    BRANCH 4.6: added support for @@ filter on add_filters

--- src/pyasm/search/sql.py
+++ src/pyasm/search/sql.py
@@ -2177,12 +2177,23 @@ class Select(object):
                 'table': table
         } )
 
+        assert op in ['in', 'not in', '@@']
+
+
+        if op == "@@":
+            # full text search requires that the filters be added one by one
+            my.add_op("begin")
+            for value in values:
+                my.add_filter(column, value, table, op)
+            my.add_op("or")
+            return
+
+
 
         if not table:
             table = my.tables[0]
 
 
-        assert op in ['in', 'not in']
         filter = ''
         if not values or values == ['']:
             if table:
