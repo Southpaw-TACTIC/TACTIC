15687112bde24f7ec2d9e95e56f1ff2c30988c65
FrankXuuu <frankxurn@gmail.com>
2018-12-31 12:20:18 -0500

    exported BaseUploadWdg, ingestWdg returns snapshots ingested

--- src/pyasm/web/monitor.py
+++ src/pyasm/web/monitor.py
@@ -495,6 +495,8 @@ class TacticSchedulerThread(threading.Thread):
         
 
         # NOTE: not sure why we have to do a batch here
+        from pyasm.security import Site
+        Site.set_site("teague")
         from pyasm.security import Batch
         Batch(login_code="admin")
 
