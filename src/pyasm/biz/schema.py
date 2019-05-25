3a07995210e3a9e0402e3819b987012454dcfa17
Sophie.Zou <zouyining@live.cn>
2019-05-25 01:32:44 -0400

    code line location change

--- src/pyasm/biz/schema.py
+++ src/pyasm/biz/schema.py
@@ -68,7 +68,6 @@ SCHEMA_XML['admin'] = '''<?xml version='1.0' encoding='UTF-8'?>
     <search_type name="sthpw/retire_log"/>
     <search_type name="sthpw/notification_log"/>
     <search_type name="sthpw/notification_login" />    
-    <connect from="sthpw/notification_login" to="sthpw/notification_log" relationship='id' from_col='notification_log_id'/>
     <search_type name="sthpw/group_notification" display="@GET(sthpw/group_notification.login_group)"/>
     <search_type name="sthpw/cache"/>
     <search_type name="sthpw/queue"/>
@@ -85,6 +84,8 @@ SCHEMA_XML['admin'] = '''<?xml version='1.0' encoding='UTF-8'?>
 
 
     <connect to="sthpw/project_type" from="sthpw/project" type='hierarchy' relationship="code" from_col='type'/>
+    
+    <connect from="sthpw/notification_login" to="sthpw/notification_log" relationship='id' from_col='notification_log_id'/>
 
     <connect from="sthpw/project" to="sthpw/db_resource" from_col="db_resource" relationship="code"/>
 
