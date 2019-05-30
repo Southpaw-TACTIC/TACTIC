fc0d55ae1440d106dd988f3a889dfb997a5ca8ba
FrankXuuu <frankxurn@gmail.com>
2019-05-30 14:23:26 -0400

    int check

--- src/pyasm/biz/task.py
+++ src/pyasm/biz/task.py
@@ -1756,16 +1756,16 @@ class TaskGenerator(object):
             duration = attrs.get("duration")
             if duration:
                 duration = int(duration)
-            elif workflow.get("duration"):
-                duration = workflow.get("duration")
+            elif workflow.get("duration") and workflow.get("duration").isdigit():
+                duration = int(workflow.get("duration"))
             else:
                 duration = default_duration
 
             bid_duration = attrs.get("bid_duration")
             if bid_duration:
                 bid_duration = int(bid_duration)
-            elif workflow.get("bid_duration"):
-                bid_duration = workflow.get("bid_duration")
+            elif workflow.get("bid_duration") and workflow.get("bid_duration").isdigit():
+                bid_duration = int(workflow.get("bid_duration"))
             else:
                 bid_duration = default_bid_duration
 
