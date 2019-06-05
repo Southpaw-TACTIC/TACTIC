313c44ee32b90889161b648d3c443f57d7d920bf
FrankXuuu <frankxurn@gmail.com>
2019-06-05 12:33:07 -0400

    style fix

--- src/tactic/ui/widget/base_sign_in_wdg.py
+++ src/tactic/ui/widget/base_sign_in_wdg.py
@@ -366,7 +366,7 @@ class ResetPasswordWdg2(BaseSignInWdg):
         div.add(code_div)
         code_div.add_class("spt_code_div")
 
-        code_div.add("<div class='msg-container'>A code was sent to <span class='msg-user'>%s<span>'s email. Please enter the code to reset your password:</div>" % login_name)
+        code_div.add("<div class='msg-container'>A code was sent to <span class='msg-user'>%s</span>'s email. Please enter the code to reset your password:</div>" % login_name)
         
         code_container = DivWdg()
         code_div.add(code_container)
