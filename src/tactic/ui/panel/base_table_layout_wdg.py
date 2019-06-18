68f85a61e865d6a9ac28766bd678e0e3ecec01ff
Swarna Datta <swarna.datta@southpawtech.com>
2019-06-18 13:33:43 -0400

    base table layout bug fix

--- src/tactic/ui/panel/base_table_layout_wdg.py
+++ src/tactic/ui/panel/base_table_layout_wdg.py
@@ -432,10 +432,10 @@ class BaseTableLayoutWdg(BaseConfigWdg):
         if self.kwargs.get('op_filters'):
             search.add_op_filters(self.kwargs.get("op_filters"))
 
-
+        values = filter_data.get_values_by_prefix("group")
+        order = WebContainer.get_web().get_form_value('order')
 
         # user-chosen order has top priority
-        order = WebContainer.get_web().get_form_value('order')
         if order:
             self.order_element = order
             if not values:
@@ -450,7 +450,6 @@ class BaseTableLayoutWdg(BaseConfigWdg):
 
 
         # passed in filter overrides
-        values = filter_data.get_values_by_prefix("group")
         if values:
 
             group_values = values[0]
