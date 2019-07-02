defa296734504adc229db05c9e568b30e2b1f63f
Celton McGrath <mcgrathcelton@gmail.com>
2019-07-02 13:22:53 -0400

    Added title for Notification Login search type, fixed update on Tab badge.

--- src/tactic/ui/container/tab_wdg.py
+++ src/tactic/ui/container/tab_wdg.py
@@ -2610,7 +2610,6 @@ spt.tab.close = function(src_el) {
                 count_wdg.add_update( {
                     'expression': count,
                     'search_key': search_key,
-                    'interval': 10,
                 } )
 
 
