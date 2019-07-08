80ed8a5a90f444b8a4cf4de7ba74026cbc78a7fe
Sophie.Zou <zouyining@live.cn>
2019-07-08 11:43:31 -0400

    remove unnecessary change

--- src/tactic/ui/panel/base_table_layout_wdg.py
+++ src/tactic/ui/panel/base_table_layout_wdg.py
@@ -1913,8 +1913,6 @@ class BaseTableLayoutWdg(BaseConfigWdg):
 
                 var offset = bvr.src_el.getPosition();
                 var size = bvr.src_el.getSize();
-                var wrapper = bvr.src_el.getParent(".wrapper");
-                var top = wrapper.getElement(".web_header");
                 offset = {x:offset.x-265, y:offset.y+size.y+10};
 
                 var body = document.id(document.body);
@@ -1922,7 +1920,7 @@ class BaseTableLayoutWdg(BaseConfigWdg):
                 var scroll_left = body.scrollLeft;
                 offset.y = offset.y - scroll_top;
                 offset.x = offset.x - scroll_left;
-                dialog.position({position: 'upperleft', relativeTo: top, offset: offset});
+                dialog.position({position: 'upperleft', relativeTo: body, offset: offset});
                 
                 spt.toggle_show_hide(dialog);
 
