63e5cb2190b8ffbde2163b0aeaf02ed8662d44c0
roonjoot <hajoon.choi@mail.utoronto.ca>
2019-07-19 09:26:44 -0500

    avoid table lock on mysql

--- src/test/run_tests.sh
+++ src/test/run_tests.sh
@@ -3,5 +3,6 @@
 files=$(find . ! -name "._*" -a -name "*test.py")
 
 while read -r line; do
-	python "$line"
+	echo "--------------- running: $line --------------------"
+    python "$line"
 done <<< "$files"
