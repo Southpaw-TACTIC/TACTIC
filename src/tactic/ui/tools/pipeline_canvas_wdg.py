4dde4844e617796ecce1367960ecea1d2ff5a19d
Shivam Thareja <shivamthareja25@gmail.com>
2019-10-22 13:55:08 -0400

    none

--- src/tactic/ui/tools/pipeline_canvas_wdg.py
+++ src/tactic/ui/tools/pipeline_canvas_wdg.py
@@ -572,7 +572,6 @@ class PipelineCanvasWdg(BaseRefreshWdg):
             var top = bvr.src_el.getParent(".spt_pipeline_top");
             if ( top.hot_key_state == false) return;
 
-            console.log(bvr.src_el);
             bvr.src_el.focus();
             '''
 
@@ -583,7 +582,6 @@ class PipelineCanvasWdg(BaseRefreshWdg):
 
 
         outer.add_behavior( {
-            
             'type': 'keyup',
             'cbjs_action': '''
             var key = evt.key;
@@ -703,7 +701,7 @@ class PipelineCanvasWdg(BaseRefreshWdg):
                 container.scale = scale;
 
             } else if (key == "n") {
-                
+
                 var canvas = spt.pipeline.get_canvas();
                 var groups = canvas.getElements(".spt_pipeline_group");
                 if (groups.length) {
@@ -802,7 +800,7 @@ class PipelineCanvasWdg(BaseRefreshWdg):
             } else if (evt.control == true && key == "v") {
 
                 var selected = spt.pipeline.get_selected_nodes();
-                
+
                 spt.pipeline.unselect_all_nodes();
                 var nodes = spt.pipeline.clipboard;
                 if (nodes) {
@@ -847,6 +845,7 @@ class PipelineCanvasWdg(BaseRefreshWdg):
 
             '''
         } )
+
 
 
 
@@ -1369,7 +1368,6 @@ class PipelineCanvasWdg(BaseRefreshWdg):
                 width: 200px;
                 height: 100px;
                 display: flex;
-                text-align:center;
                 align-items: center;
                 justify-content: center;
 
@@ -1434,7 +1432,7 @@ class PipelineCanvasWdg(BaseRefreshWdg):
 
         #group_div.add_style("font-weight: bold")
 
-        button = DivWdg("Click here !!!!!!!!!!! 222222to add your first node. Use the <i class='fa fa-wrench'></i> to add more nodes.")
+        button = DivWdg("Click here to add your first node. Use the <i class='fa fa-wrench'></i> to add more nodes.")
         content_div.add( button )
 
 
@@ -4420,7 +4418,6 @@ spt.pipeline.get_all_folders = function() {
 }
 
 spt.pipeline.add_folder = function(group_name, color, title) {
-
     if (typeof(color) == 'undefined') {
         color = '#999';
     }
@@ -5824,7 +5821,6 @@ spt.pipeline.fit_to_canvas = function(group_name) {
     var nodes = null;
     if (typeof(group_name) == 'undefined') {
         nodes = spt.pipeline.get_all_nodes();
-        folder = spt.pipeline.get_all_folders();
     }
     else {
         nodes = spt.pipeline.get_nodes_by_group(group_name);
@@ -5836,10 +5832,12 @@ spt.pipeline.fit_to_canvas = function(group_name) {
     var left = null;
     var bottom = null;
     var right = null;
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].getStyle("display") == "none") {
+            continue;
+        }
 
-    if(nodes.length == 0){
-
-        var pos = spt.pipeline.get_position(folder[0]);
+        var pos = spt.pipeline.get_position(nodes[i]);
         if (left == null || pos.x < left) {
             left = pos.x;
         }
@@ -5852,29 +5850,6 @@ spt.pipeline.fit_to_canvas = function(group_name) {
         if (bottom == null || pos.y > bottom) {
             bottom = pos.y
         }
-
-
-        }
-        else{
-            for (var i = 0; i < nodes.length; i++) {
-                if (nodes[i].getStyle("display") == "none") {
-                    continue;
-                }
-
-                var pos = spt.pipeline.get_position(nodes[i]);
-                if (left == null || pos.x < left) {
-                    left = pos.x;
-                }
-                if (top == null || pos.y < top) {
-                    top = pos.y;
-                }
-                if (right == null || pos.x > right) {
-                    right = pos.x
-                }
-                if (bottom == null || pos.y > bottom) {
-                    bottom = pos.y
-                }
-            }
     }
 
     var size = spt.pipeline.get_canvas_size();
