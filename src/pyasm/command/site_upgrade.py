07e5da7f39ddbee2bd58743c32afd75367deef03
Sophie.Zou <zouyining@live.cn>
2019-10-26 00:10:41 -0400

    fixing wrong code commited

--- src/pyasm/command/site_upgrade.py
+++ src/pyasm/command/site_upgrade.py
@@ -15,18 +15,24 @@ class SiteUpgradeCmd(Command):
     def execute(self):
         '''Update the Plugins and Database of this project'''
         sudo = Sudo()
-        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-        print(self.kwargs)
+
         project_code = self.kwargs.get("project_code")
         site = self.kwargs.get("site")
-        is_upgrade = self.kwargs.get("is_upgrade") or [None]
+        is_upgraded = False
+
+        Site.set_site(site)
+
+        search = Search('config/plugin')
+        plugin_sobjects = search.get_sobjects()
+
 
         Project.set_project('sthpw')
         db_resource = Project.get().get_project_db_resource()
         db = Sql(db_resource)
-    
-        update_sql = "select last_version_update from project where code = '%s';" % project_code
-        project_version = db.do_query(update_sql)
+
+        project_sobject = Search.eval("@SOBJECT(sthpw/project['code', '%s'])" % project_code)
+        print(project_sobject)
+        project_version = project_sobject[0].get_value("last_version_update")
         newest_version = Environment.get_release_version()
 
         if newest_version[0] == 'v':
@@ -34,15 +40,12 @@ class SiteUpgradeCmd(Command):
         
         if project_version:
             if project_version != newest_version:
+                is_upgraded = True
                 os.system("python /spt/tactic/TACTIC/src/bin/upgrade_db.py -y -f -p %s -s %s" % (project_code, site))
 
-        Project.set_project(project_code)
-        project_db_resource = Project.get().get_project_db_resource()
-        project_db = Sql(project_db_resource)
-        plugin_sql = 'select code, version from spt_plugin'
-        plugins = copy(project_db.do_query(plugin_sql))
-        
-        for code, version in plugins:
+        for plugin_sobject in plugin_sobjects:
+            code = plugin_sobject.get_value("code")
+            version = plugin_sobject.get_value("version")
             if "stypes" in code:
                 continue
             plugin_dir = '%s/%s' % (Environment.get_plugin_dir(), code)
@@ -63,6 +66,7 @@ class SiteUpgradeCmd(Command):
                 if not latest_version:
                     continue
                 elif not version:
+                    is_upgraded = True
                     print("Uninstalling plugin: ", plugin_dir)
                     uninstaller = PluginUninstaller(plugin_dir=plugin_dir, verbose=False)
                     uninstaller.execute()
@@ -74,8 +78,8 @@ class SiteUpgradeCmd(Command):
                     log_f = open(log_path, 'a')
                     log_f.write("Plugin Updated to version %s: %s\n" % (latest_version, datetime.datetime.now()))
                     log_f.close()
-                    is_upgrade[0] = True
                 elif version != latest_version:
+                    is_upgraded = True
                     print("Uninstalling plugin: ", plugin_dir)
                     uninstaller = PluginUninstaller(plugin_dir=plugin_dir, verbose=False)
                     uninstaller.execute()
@@ -87,5 +91,13 @@ class SiteUpgradeCmd(Command):
                     log_f = open(log_path, 'a')
                     log_f.write("Plugin Updated to version %s: %s\n" % (latest_version, datetime.datetime.now()))
                     log_f.close()
-                    is_upgrade[0] = True
+            
+        tmp_dir = Environment.get_tmp_dir()
+        filename = "upgrade/upgrade_%s.txt" % site
+        f = open("%s/%s" % (tmp_dir, filename), "w")
+        if is_upgraded:
+            f.write("true")
+        else:
+            f.write('false')
+        f.close()
         sudo.exit()
