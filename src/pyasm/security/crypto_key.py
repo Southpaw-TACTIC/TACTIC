1ca9197faf7d7e585087fa522e14afd5357208b5
Celton McGrath <mcgrathcelton@gmail.com>
2019-11-04 13:26:10 -0500

    Fixed Crypto functions

--- src/pyasm/security/crypto_key.py
+++ src/pyasm/security/crypto_key.py
@@ -16,16 +16,21 @@ __all__ = ['CryptoKey']
 import os, binascii
 
 try:
+    from Cryptodome.PublicKey import RSA
     from Cryptodome.Signature import pkcs1_15
     from Cryptodome.Hash import MD5, SHA256
-    from Cryptodome.PublicKey import RSA
     from Cryptodome.Cipher import PKCS1_OAEP
+    HAS_CRYPTODOME = True
 except ImportError:
     from Crypto.Hash import MD5
     from Crypto.PublicKey import RSA
+    HAS_CRYPTODOME = False
+except ImportError:
+    raise Exception("Must install Python Cryptodome library")
 
 
 class CryptoKey(object):
+    
     def __init__(self):
         self.key = None
         self.private_key = None
@@ -69,22 +74,19 @@ class CryptoKey(object):
 
 
     def get_signature(self, msg):
-        try:
-            hash = MD5.new(msg.encode()).digest()
-            signature = self.key.sign(hash, "")
-        except (TypeError, NotImplementedError) as e:
+        if HAS_CRYPTODOME:
             msg = msg.encode('utf-8')
             h = SHA256.new(msg)
             signature = pkcs1_15.new(self.key).sign(h)
+        else:
+            hash = MD5.new(msg.encode()).digest()
+            signature = self.key.sign(hash, "")
         
         return signature
 
 
     def verify(self, msg, signature):
-        try:
-            hash = MD5.new(msg).digest()
-            return self.key.verify(hash, signature)
-        except (TypeError, NotImplementedError) as e:
+        if HAS_CRYPTODOME:
             msg = msg.encode('utf-8')
             h = SHA256.new(msg)
             try:
@@ -92,33 +94,36 @@ class CryptoKey(object):
                 return True
             except ValueError as e:
                 return False
+        else:
+            hash = MD5.new(msg).digest()
+            return self.key.verify(hash, signature)
 
 
 
     def encrypt(self, msg):
-        try:
+        if HAS_CRYPTODOME:
+            encryptor = PKCS1_OAEP.new(self.key)
+            encrypted = encryptor.encrypt(msg.encode())
+            return encrypted
+        else:
             k = 84744 # A random parameter (for compatibility only. This value will be ignored) 
             coded = self.key.encrypt(msg.encode(), k) # encode required for Python3
             hex = binascii.hexlify(coded[0])
             return hex
-        except NotImplementedError:
-            encryptor = PKCS1_OAEP.new(self.key)
-            encrypted = encryptor.encrypt(msg.encode())
-            return encrypted
 
 
 
 
     def decrypt(self, ciphertext):
-        try:
-            uncoded = binascii.unhexlify(ciphertext)
-            decrypt = self.key.decrypt(uncoded)
-            decrypt = decrypt.decode('ascii')
-            return decrypt
-        except binascii.Error as e:
+        if HAS_CRYPTODOME:
             cipher = PKCS1_OAEP.new(self.key)
             decrypt = cipher.decrypt(ciphertext)
             decrypt = decrypt.decode()
+            return decrypt
+        else:
+            uncoded = binascii.unhexlify(ciphertext)
+            decrypt = self.key.decrypt(uncoded)
+            decrypt = decrypt.decode('ascii')
             return decrypt
 
 
