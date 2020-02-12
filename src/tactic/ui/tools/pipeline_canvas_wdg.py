c73405275d9b375d1f0281bcf7fab3a3bdaf649d
Lwin Moe <admin@lwinmoe.org>
2020-02-12 16:31:32 -0500

    Load Detail Report link removed from Pipeline Wdg.

--- src/tactic/ui/tools/pipeline_canvas_wdg.py
+++ src/tactic/ui/tools/pipeline_canvas_wdg.py
@@ -890,21 +890,22 @@ class PipelineCanvasWdg(BaseRefreshWdg):
 
 
         process_menu = self.get_node_context_menu()
-        menus = [process_menu.get_data()]
+        if process_menu:
+            menus = [process_menu.get_data()]
 
-        # Simple context menu is for renaming and
-        # deleting approval, action and condition nodes..
-        simple_menu = self.get_simple_node_context_menu()
-        simple_menus = [simple_menu.get_data()]
+            # Simple context menu is for renaming and
+            # deleting approval, action and condition nodes..
+            simple_menu = self.get_simple_node_context_menu()
+            simple_menus = [simple_menu.get_data()]
 
-        menus_in = {
-            'NODE_CTX': menus,
-            'SIMPLE_NODE_CTX': simple_menus
-        }
+            menus_in = {
+                'NODE_CTX': menus,
+                'SIMPLE_NODE_CTX': simple_menus
+            }
 
-        if self.is_editable == True:
-            from tactic.ui.container.smart_menu_wdg import SmartMenu
-            SmartMenu.attach_smart_context_menu( outer, menus_in, False )
+            if self.is_editable == True:
+                from tactic.ui.container.smart_menu_wdg import SmartMenu
+                SmartMenu.attach_smart_context_menu( outer, menus_in, False )
 
         # inner is used to scale
         inner = DivWdg()
