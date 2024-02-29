b956ffca8732fde383d0bff4e0409a575e1f6655
TACTIC <tactic@localhost.localdomain>
2024-02-29 16:14:04 -0500

    More fixes on loading rect libraries

--- src/tactic/react/react.py
+++ src/tactic/react/react.py
@@ -7,32 +7,35 @@ class React():
 
     @classmethod
     def load_libraries(cls, widget):
+
+        from tactic.ui.tools import ReactWdg
+
         tactic_src_dir = tacticenv.get_install_dir()
         react_dir = "%s/src/tactic/ui/react" % tactic_src_dir
 
         jsx_path = "%s/redux/store.jsx" % dirname
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
 
         # Resource central components
         jsx_path = "%s/common.jsx" % react_dir
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
 
         jsx_path = "%s/data_grid.jsx" % react_dir
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
         jsx_path = "%s/widget/notes.jsx" % react_dir
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
         jsx_path = "%s/import_data.jsx" % tactic_react_dir
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
         jsx_path = "%s/table_layout.jsx" % react_dir
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
         jsx_path = "%s/config.jsx" % react_dir
-        self.init_react(widget, jsx_path)
+        ReactWdg.init_react(widget, jsx_path)
 
 
 
