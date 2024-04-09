172cb2476c45028559233b3ca0b7e31ad9591970
TACTIC <tactic@localhost.localdomain>
2024-04-09 13:02:32 -0400

    Upgraded Mootools to 1.6

--- src/tactic/ui/app/top_wdg.py
+++ src/tactic/ui/app/top_wdg.py
@@ -1116,18 +1116,20 @@ class TopWdg(Widget):
         ''')
 
         # JQuery
-        """
-        widget.add('''
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        ''')
-        """
-        widget.add('''
-<script src="/context/spt_js/jquery/jquery-3.4.1.min.js"></script>
-<script src="/context/spt_js/jquery/jquery-ui.min.js"></script>
-<script src="/context/spt_js/jquery/popper.min.js"></script>
-        ''')
+        include_jquery = Config.get_value("install", "include_jquery") or "true"
+        if include_jquery == "true":
+            """
+            widget.add('''
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            ''')
+            """
+            widget.add('''
+    <script src="/context/spt_js/jquery/jquery-3.4.1.min.js"></script>
+    <script src="/context/spt_js/jquery/jquery-ui.min.js"></script>
+    <script src="/context/spt_js/jquery/popper.min.js"></script>
+            ''')
  
         use_formio = True
         if use_formio:
@@ -1231,6 +1233,9 @@ class TopWdg(Widget):
               <script src="/context/react/redux.js" crossorigin></script>
               <script src="/context/react/redux-toolkit.umd.min.js" crossorigin></script>
               <script src="/context/react/react-redux.min.js" crossorigin></script>
+
+<script src="/context/react/ag-grid-community.min.noStyle.js?ver=5.0.0.a01" ></script>
+<script src="#https://cdn.jsdelivr.net/npm/ag-charts-community/dist/ag-charts-community.min.js?ver=5.0.0.a01" ></script>
             ''')
 
 
